d0a6bc547dec060de2c750b0e7b6975946d92b0e
Tyler Hevia <tylerhevia@gmail.com>
2017-10-01 21:55:35 -0600

    completed exercise 2

--- promises.js
+++ promises.js
@@ -9,3 +9,32 @@ function testNum(num) {
     }
   });
 }
+
+testNum(9)
+  .then(result => console.log(result))
+  .catch(error => console.log(error));
+
+// Exercise 2
+
+let desserts = ["pie", "ice cream", "cake", "brownies", "apples", "delicious"];
+
+function makeAllCaps(things) {
+  return new Promise((resolve, reject) => {
+    if (things.every(item => typeof item === "string")) {
+      resolve(things.map(item => item.toUpperCase()));
+    } else {
+      reject(
+        console.log(
+          "No, the array you passed in contained an element that was not a string!"
+        )
+      );
+    }
+  });
+}
+
+const sortWords = words => words.sort();
+
+makeAllCaps(desserts)
+  .then(res => sortWords(res))
+  .then(res => console.log(res))
+  .catch(error => console.log(error));
